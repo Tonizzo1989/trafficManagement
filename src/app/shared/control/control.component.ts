ff0e6b972363bbe753c3b72a36bdc05df4eec0d7
Tonino <toninorizzo1989@yahoo.it>
2024-07-25 17:07:58 +0200

    added ngOnInit

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,14 +1,36 @@
-import { Component, input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
+  /* @HostBinding('class') className = 'control'; */
+  /* @HostListener('click') onClick() {
+    console.log('Clicked');
+  } */
 
+  private el = inject(ElementRef);
   label = input.required<string>();
 
+  onClick() {
+    console.log('Clicked');
+    console.log(this.el);
+  }
 }
